7d6695d94330b93f9cd1b6044ffa13f8a7bae300
chei7tain <fredrickokwuobi@gmail.com>
2022-01-05 23:06:10 +0100

    add Header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Header from "./components/Header";
 //styles
 import GlobalStyle from "./GlobalStyle";
 function App() {
   return (
     <div className="App">
+      <Header />
       Start Here
       <GlobalStyle />
     </div>
