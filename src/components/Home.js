972d3af93acdf0cd578e1757d67594da2ea343fb
chei7tain <fredrickokwuobi@gmail.com>
2022-01-06 15:36:27 +0100

    fetched data from movie data api using useEffect Hool

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
+import API from "../API";
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 //Components
 
@@ -12,6 +12,31 @@ const Home = () => {
   const [state, setState] = useState();
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
+
+  const fetchMovies = async (page, searchTerm = "") => {
+    try {
+      setError(false);
+      setLoading(true);
+
+      const movies = await API.fetchMovies(searchTerm, page);
+      console.log(movies);
+
+      setState((prev) => ({
+        ...movies,
+        results:
+          page > 1 ? [...prev.results, ...movies.results] : movies.results,
+      }));
+    } catch (error) {
+      setError(true);
+    }
+    setLoading(false);
+  };
+
+  //Initial render
+  useEffect(() => {
+    fetchMovies(1);
+  }, []);
+  console.log(state);
   return <div className="">Home page</div>;
 };
 
