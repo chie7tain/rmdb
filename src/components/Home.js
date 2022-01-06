0a8a485382c445afaf53acd2c8524b9537b7108b
chei7tain <fredrickokwuobi@gmail.com>
2022-01-06 16:07:14 +0100

    add Hero Image component and styles

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import API from "../API";
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 //Components
-
+import HeroImage from "./HeroImage";
 //Hook
 import { useHomeFetch } from "../Hooks/useHomeFetch";
 //Image
@@ -12,7 +12,17 @@ const Home = () => {
   const { state, loading, error } = useHomeFetch();
 
   console.log(state);
-  return <div className="">Home page</div>;
+  return (
+    <>
+      {state.results[0] && (
+        <HeroImage
+          image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+          title={state.results[0].original_title}
+          text={state.results[0].overview}
+        />
+      )}
+    </>
+  );
 };
 
 export default Home;
