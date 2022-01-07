06b8ea387558ae52bdb12e02da4e40684a65a589
chei7tain <fredrickokwuobi@gmail.com>
2022-01-07 21:36:29 +0100

    add Button component and imported it into the Home component and conditionally render it using js

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,6 +7,7 @@ import Grid from "./Grid";
 import Thumb from "./Thumb";
 import Spinner from "./Spinner";
 import SearchBar from "./SearchBar";
+import Button from "./Button";
 ///Hook
 import useHomeFetch from "../Hooks/useHomeFetch";
 
@@ -40,7 +41,10 @@ const Home = () => {
           />
         ))}
       </Grid>
-      <Spinner />
+      {loading && <Spinner />}
+      {state.page < state.total_pages && !loading && (
+        <Button text="Load More" />
+      )}
     </>
   );
 };
