51981fab71336cc27e2e9aa2239b7bc343f361d2
chei7tain <fredrickokwuobi@gmail.com>
2022-01-07 18:00:07 +0100

    impl the HeroImage component and imported it to the Home component where it would receive the following props: image, title and text

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,8 @@
-import React, { useState, useEffect } from "react";
-
 ///config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 
 ///components
-
+import HeroImage from "./HeroImage";
 ///Hook
 import { useHomeFetch } from "../hooks/useHomeFetch";
 
@@ -14,6 +12,16 @@ import NoImage from "../images/no_image.jpg";
 const Home = () => {
   const { state, loading, error } = useHomeFetch();
   console.log(state);
-  return <div className="">Home Page</div>;
+  return (
+    <>
+      {state.results[0] ? (
+        <HeroImage
+          image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+          title={state.results[0].original_title}
+          text={state.results[0].overview}
+        />
+      ) : null}
+    </>
+  );
 };
 export default Home;
