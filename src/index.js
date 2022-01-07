593b4de114b1012626ec909ddf06427147978aa2
chei7tain <fredrickokwuobi@gmail.com>
2022-01-08 00:44:35 +0100

    add .env-cmdrc.json file to hold scrts

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+// import dotenv from "dotenv";
+// dotenv.config();
+
+console.log(process.env);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
