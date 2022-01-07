ec8870c9bbdbc9bd5c667f113ff62a97228194a6
chei7tain <fredrickokwuobi@gmail.com>
2022-01-07 17:25:06 +0100

    fetched data in Home using use

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
-
+///API
+import API from "../API";
 ///config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 
@@ -15,6 +16,28 @@ const Home = () => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
 
+  const fetchMovies = async (page, searchTerm = "") => {
+    try {
+      setError(false);
+      setLoading(true);
+
+      const movies = await API.fetchMovies(searchTerm, page);
+      setState((prev) => ({
+        ...movies,
+        results:
+          page > 1 ? [...prev.results, ...movies.results] : [...movies.results],
+      }));
+    } catch (error) {
+      setError(true);
+    }
+    setLoading(false);
+  };
+
+  ///initial render
+  useEffect(() => {
+    fetchMovies(1);
+  }, []);
+  console.log(state);
   return <div className="">Home Page</div>;
 };
 export default Home;
