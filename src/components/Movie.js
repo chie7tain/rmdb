4fcc056dea1345539566115e7dcdaf0fbea03464
chei7tain <fredrickokwuobi@gmail.com>
2022-01-08 23:15:44 +0100

    created a useMovieFetch hook to fetch movie data

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,6 +1,24 @@
-import React from "react"
+import React from "react";
+import { useParams } from "react-router-dom";
+//config
+import { IMAGE_BASE_URL, POSTER_SIZE } from "../config";
+//Components
+import Grid from "./Grid";
+import Spinner from "./Spinner";
 
-const Movie = ()=>(
-  <div>Movie</div>
-)
+//Hooks
+import { useMovieFetch } from "../Hooks/useMovieFetch";
+//image
+import NoImage from "../images/no_image.jpg";
+
+const Movie = () => {
+  const { movieId } = useParams();
+  const { state: movie, loading, error } = useMovieFetch(movieId);
+  console.log(movie);
+  return (
+    <>
+      <div>Movie</div>
+    </>
+  );
+};
 export default Movie;
